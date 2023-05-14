0e945e50bd4103f963c5b75271dfec2c82109f45
kavysabu1996 <annkavya1996@gmail.com>
2023-05-14 20:15:27 +0530

    added utils.py, position_embeddings.py and linear_layer.py

---
+++ linear_layer.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+
+class Linear(tf.keras.layers.Layer):
+    def __init__(self,weight,bias,layer_name):
+        super().__init__()
+        self.num_filters = bias.shape[0]
+        self.weight = weight
+        self.bias = bias
+        self.layer_name = layer_name
+
+    def call(self,inputs):
+        matmul = tf.keras.layers.Conv2D(filters=self.num_filters, kernel_size = (1,1), strides =1, name=self.layer_name)
+        tmp = matmul(np.zeros(inputs.shape))
+        matmul.set_weights([self.weight,self.bias])
+        return matmul(inputs)
+
